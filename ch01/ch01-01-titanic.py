123435890dcbf8155d328c881726f064aa7891b4
rilts <rialtoksj@yahoo.com>
2020-12-16 08:15:14 +0900

    한글번역

--- ch01/ch01-01-titanic.py
+++ ch01/ch01-01-titanic.py
@@ -2,220 +2,220 @@ import numpy as np
 import pandas as pd
 
 # -----------------------------------
-# 学習データ、テストデータの読み込み
+# 학습데이터, 테스트데이터 읽어들이기
 # -----------------------------------
-# 学習データ、テストデータの読み込み
+# 학습데이터, 테스트데이터 읽어들이기
 train = pd.read_csv('../input/ch01-titanic/train.csv')
 test = pd.read_csv('../input/ch01-titanic/test.csv')
 
-# 学習データを特徴量と目的変数に分ける
+# 학습데이터를 특징량과 목적변수로 나누기
 train_x = train.drop(['Survived'], axis=1)
 train_y = train['Survived']
 
-# テストデータは特徴量のみなので、そのままでよい
+# 테스트데이터는 특징량만 있기에 이대로 둠
 test_x = test.copy()
 
 # -----------------------------------
-# 特徴量作成
+# 특징량 작성
 # -----------------------------------
 from sklearn.preprocessing import LabelEncoder
 
-# 変数PassengerIdを除外する
+# 변수PassengerId를 제외한다.
 train_x = train_x.drop(['PassengerId'], axis=1)
 test_x = test_x.drop(['PassengerId'], axis=1)
 
-# 変数Name, Ticket, Cabinを除外する
+# 변수Name, Ticket, Cabin을 제외한다.
 train_x = train_x.drop(['Name', 'Ticket', 'Cabin'], axis=1)
 test_x = test_x.drop(['Name', 'Ticket', 'Cabin'], axis=1)
 
-# それぞれのカテゴリ変数にlabel encodingを適用する
+# 각각의 범주변수에 label encoding을 적용한다.
 for c in ['Sex', 'Embarked']:
-    # 学習データに基づいてどう変換するかを定める
+    # 학습 데이터에 기반하여 어떻게 변환할 지를 정한다.
     le = LabelEncoder()
     le.fit(train_x[c].fillna('NA'))
 
-    # 学習データ、テストデータを変換する
+    # 학습데이터, 테스트데이터를 변환한다.
     train_x[c] = le.transform(train_x[c].fillna('NA'))
     test_x[c] = le.transform(test_x[c].fillna('NA'))
 
 # -----------------------------------
-# モデル作成
+# 모델작성
 # -----------------------------------
 from xgboost import XGBClassifier
 
-# モデルの作成および学習データを与えての学習
+# 모델 작성 및 학습데이터를 학습
 model = XGBClassifier(n_estimators=20, random_state=71)
 model.fit(train_x, train_y)
 
-# テストデータの予測値を確率で出力する
+# 테스트데이터의 예측값을 확률로 출력한다.
 pred = model.predict_proba(test_x)[:, 1]
 
-# テストデータの予測値を二値に変換する
+# 테스트데이터의 예측값을 2진값으로 변환한다.
 pred_label = np.where(pred > 0.5, 1, 0)
 
-# 提出用ファイルの作成
+# 제출용 파일 작성
 submission = pd.DataFrame({'PassengerId': test['PassengerId'], 'Survived': pred_label})
 submission.to_csv('submission_first.csv', index=False)
-# スコア：0.7799（本書中の数値と異なる可能性があります）
+# 스코어：0.7799（책과 수치가 다를 가능성이 있습니다）
 
 # -----------------------------------
-# バリデーション
+# 밸리데이션(검증)
 # -----------------------------------
 from sklearn.metrics import log_loss, accuracy_score
 from sklearn.model_selection import KFold
 
-# 各foldのスコアを保存するリスト
+# 각fold의 스코어를 저장하는 리스트
 scores_accuracy = []
 scores_logloss = []
 
-# クロスバリデーションを行う
-# 学習データを4つに分割し、うち1つをバリデーションデータとすることを、バリデーションデータを変えて繰り返す
+# 크로스밸리데이션을 실행한다.
+# 학습데이터를 4개로 분할하고 그 중 하나를 밸리데이션데이터로 하고 밸리데이션 데이터를 바꿔가며 반복한다.
 kf = KFold(n_splits=4, shuffle=True, random_state=71)
 for tr_idx, va_idx in kf.split(train_x):
-    # 学習データを学習データとバリデーションデータに分ける
+    # 학습데이터를 학습데이터와 밸리데이션데이터로 나눈다.
     tr_x, va_x = train_x.iloc[tr_idx], train_x.iloc[va_idx]
     tr_y, va_y = train_y.iloc[tr_idx], train_y.iloc[va_idx]
 
-    # モデルの学習を行う
+    # 모델의 학습을 진행한다.
     model = XGBClassifier(n_estimators=20, random_state=71)
     model.fit(tr_x, tr_y)
 
-    # バリデーションデータの予測値を確率で出力する
+    # 밸리데이션데이터의 예측값을 활률로 출력한다.
     va_pred = model.predict_proba(va_x)[:, 1]
 
-    # バリデーションデータでのスコアを計算する
+    # 밸리데이션데이터로 스코어를 계산한다.
     logloss = log_loss(va_y, va_pred)
     accuracy = accuracy_score(va_y, va_pred > 0.5)
 
-    # そのfoldのスコアを保存する
+    # 그 fold의 스코어를 저장한다.
     scores_logloss.append(logloss)
     scores_accuracy.append(accuracy)
 
-# 各foldのスコアの平均を出力する
+# 각 fold의 스코어의 평균을 출력한다.
 logloss = np.mean(scores_logloss)
 accuracy = np.mean(scores_accuracy)
 print(f'logloss: {logloss:.4f}, accuracy: {accuracy:.4f}')
-# logloss: 0.4270, accuracy: 0.8148（本書中の数値と異なる可能性があります）
+# logloss: 0.4270, accuracy: 0.8148（책과 수치가 다를 가능성이 있습니다）
 
 # -----------------------------------
-# モデルチューニング
+# 모델 튜닝
 # -----------------------------------
 import itertools
 
-# チューニング候補とするパラメータを準備する
+# 튜닝 후보로 할 파라메터를 준비한다.
 param_space = {
     'max_depth': [3, 5, 7],
     'min_child_weight': [1.0, 2.0, 4.0]
 }
 
-# 探索するハイパーパラメータの組み合わせ
+# 탐색할 하이퍼파라메터의 조합
 param_combinations = itertools.product(param_space['max_depth'], param_space['min_child_weight'])
 
-# 各パラメータの組み合わせ、それに対するスコアを保存するリスト
+# 각 파라메터의 조합에 대한 스코어를 저장하는 리스트
 params = []
 scores = []
 
-# 各パラメータの組み合わせごとに、クロスバリデーションで評価を行う
+# 각 파라메서 조합마다 크로스밸리데이션으로 평가를 진행한다.
 for max_depth, min_child_weight in param_combinations:
 
     score_folds = []
-    # クロスバリデーションを行う
-    # 学習データを4つに分割し、うち1つをバリデーションデータとすることを、バリデーションデータを変えて繰り返す
+    # 크로스밸리데이션을 진행한다.
+    # 학습데이터를 4개로 분할하고 그 중 하나를 밸리데이션데이터로 하고 밸리데이션 데이터를 바꿔가며 반복한다.
     kf = KFold(n_splits=4, shuffle=True, random_state=123456)
     for tr_idx, va_idx in kf.split(train_x):
-        # 学習データを学習データとバリデーションデータに分ける
+        # 학습데이터를 학습데이터와 밸리데이션데이터로 나눈다.
         tr_x, va_x = train_x.iloc[tr_idx], train_x.iloc[va_idx]
         tr_y, va_y = train_y.iloc[tr_idx], train_y.iloc[va_idx]
 
-        # モデルの学習を行う
+        # 모델의 학습을 진행한다.
         model = XGBClassifier(n_estimators=20, random_state=71,
                               max_depth=max_depth, min_child_weight=min_child_weight)
         model.fit(tr_x, tr_y)
 
-        # バリデーションデータでのスコアを計算し、保存する
+        # 밸리데이션데이터로 스코어를 계산하고 저장한다.
         va_pred = model.predict_proba(va_x)[:, 1]
         logloss = log_loss(va_y, va_pred)
         score_folds.append(logloss)
 
-    # 各foldのスコアを平均する
+    # 각 fold의 스코어의 평균을 계산한다.
     score_mean = np.mean(score_folds)
 
-    # パラメータの組み合わせ、それに対するスコアを保存する
+    # 파라메터의 조합에 대한 스코어를 저장한다.
     params.append((max_depth, min_child_weight))
     scores.append(score_mean)
 
-# 最もスコアが良いものをベストなパラメータとする
+# 가장 스코어가 좋은 것을 베스트 파라메터로 한다.
 best_idx = np.argsort(scores)[0]
 best_param = params[best_idx]
 print(f'max_depth: {best_param[0]}, min_child_weight: {best_param[1]}')
-# max_depth=7, min_child_weight=2.0のスコアが最もよかった
+# max_depth=7, min_child_weight=2.0 의 스코어가 가장 좋았다.
 
 
 # -----------------------------------
-# ロジスティック回帰用の特徴量の作成
+# 로지스틱회귀용 특징량 작성
 # -----------------------------------
 from sklearn.preprocessing import OneHotEncoder
 
-# 元データをコピーする
+# 원데이터를 복사한다.
 train_x2 = train.drop(['Survived'], axis=1)
 test_x2 = test.copy()
 
-# 変数PassengerIdを除外する
+# 변수PassengerId를 제외한다.
 train_x2 = train_x2.drop(['PassengerId'], axis=1)
 test_x2 = test_x2.drop(['PassengerId'], axis=1)
 
-# 変数Name, Ticket, Cabinを除外する
+# 변수Name, Ticket, Cabin을 제외한다.
 train_x2 = train_x2.drop(['Name', 'Ticket', 'Cabin'], axis=1)
 test_x2 = test_x2.drop(['Name', 'Ticket', 'Cabin'], axis=1)
 
-# one-hot encodingを行う
+# one-hot encoding을 실행한다.
 cat_cols = ['Sex', 'Embarked', 'Pclass']
 ohe = OneHotEncoder(categories='auto', sparse=False)
 ohe.fit(train_x2[cat_cols].fillna('NA'))
 
-# one-hot encodingのダミー変数の列名を作成する
+# one-hot encoding의 더미변수의 열명을 작성한다.
 ohe_columns = []
 for i, c in enumerate(cat_cols):
     ohe_columns += [f'{c}_{v}' for v in ohe.categories_[i]]
 
-# one-hot encodingによる変換を行う
+# one-hot encoding에 의한 변환을 실행한다.
 ohe_train_x2 = pd.DataFrame(ohe.transform(train_x2[cat_cols].fillna('NA')), columns=ohe_columns)
 ohe_test_x2 = pd.DataFrame(ohe.transform(test_x2[cat_cols].fillna('NA')), columns=ohe_columns)
 
-# one-hot encoding済みの変数を除外する
+# one-hot encoding이 완료된 변수를 제외한다.
 train_x2 = train_x2.drop(cat_cols, axis=1)
 test_x2 = test_x2.drop(cat_cols, axis=1)
 
-# one-hot encodingで変換された変数を結合する
+# one-hot encoding으로 변환된 변수를 결합한다.
 train_x2 = pd.concat([train_x2, ohe_train_x2], axis=1)
 test_x2 = pd.concat([test_x2, ohe_test_x2], axis=1)
 
-# 数値変数の欠損値を学習データの平均で埋める
+# 수치변수의 결측값을 학습데이터의 평균으로 채운다.
 num_cols = ['Age', 'SibSp', 'Parch', 'Fare']
 for col in num_cols:
     train_x2[col].fillna(train_x2[col].mean(), inplace=True)
     test_x2[col].fillna(train_x2[col].mean(), inplace=True)
 
-# 変数Fareを対数変換する
+# 변수Fare를 log로 변환한다.
 train_x2['Fare'] = np.log1p(train_x2['Fare'])
 test_x2['Fare'] = np.log1p(test_x2['Fare'])
 
 # -----------------------------------
-# アンサンブル
+# 앙상블
 # -----------------------------------
 from sklearn.linear_model import LogisticRegression
 
-# xgboostモデル
+# xgboost모델
 model_xgb = XGBClassifier(n_estimators=20, random_state=71)
 model_xgb.fit(train_x, train_y)
 pred_xgb = model_xgb.predict_proba(test_x)[:, 1]
 
-# ロジスティック回帰モデル
-# xgboostモデルとは異なる特徴量を入れる必要があるので、別途train_x2, test_x2を作成した
+# 로지스틱회귀모델
+# xgboost모델과는 다른 특징량을 넣을 필요가 있기 때문에 별도의 train_x2, test_x2를 작성했다.
 model_lr = LogisticRegression(solver='lbfgs', max_iter=300)
 model_lr.fit(train_x2, train_y)
 pred_lr = model_lr.predict_proba(test_x2)[:, 1]
 
-# 予測値の加重平均をとる
+# 예측값의 가중평균을 구한다.
 pred = pred_xgb * 0.8 + pred_lr * 0.2
 pred_label = np.where(pred > 0.5, 1, 0)
--- readme.md
+++ readme.md
@@ -1,43 +1,43 @@
-## サンプルコード
+## 샘플코드
 
-「Kaggleで勝つデータ分析の技術」([amazon](https://www.amazon.co.jp/dp/4297108437)) のサンプルコードです。
+「Kaggle에서 우승하는 데이터분석의 기술」([amazon](https://www.amazon.co.jp/dp/4297108437)) 의 샘플코드입니다.
 
 <img src="misc/cover_small.jpg" width="200">
 
-### 各フォルダの内容
+### 각 폴더의 내용
 
-|フォルダ| 内容 |
+|폴더| 내용 |
 |:----|:-------|
-| input | 入力ファイル |
-| ch01 | 第1章のサンプルコード |
-| ch02 | 第2章のサンプルコード |
-| ch03 | 第3章のサンプルコード |
-| ch04 | 第4章のサンプルコード |
-| ch05 | 第5章のサンプルコード |
-| ch06 | 第6章のサンプルコード |
-| ch07 | 第7章のサンプルコード |
-| ch04-model-interface | 第4章の「分析コンペ用のクラスやフォルダの構成」のコード |
+| input | 입력파일 |
+| ch01 | 제1장의 샘플코드 |
+| ch02 | 제2장의 샘플코드 |
+| ch03 | 제3장의 샘플코드 |
+| ch04 | 제4장의 샘플코드 |
+| ch05 | 제5장의 샘플코드 |
+| ch06 | 제6장의 샘플코드 |
+| ch07 | 제7장의 샘플코드 |
+| ch04-model-interface | 제4장의「분석대회용 클래스와 폴더의 구성」의 코드 |
 
-* 各章のディレクトリをカレントディレクトリとしてコードを実行して下さい。
-* 第1章のタイタニックのデータは、[input/readme.md](input/readme.md) のとおりダウンロード下さい。
-* 第4章の「分析コンペ用のクラスやフォルダの構成」のコードについては、[ch04-model-interface/readme.md](ch04-model-interface) を参照下さい。
+* 각장의 디렉토리를 현재디렉토리로 해서 코드를 실행해 주세요.
+* 제1장의 타이타닉데이터는 [input/readme.md](input/readme.md)에 적힌대로 다운로드해 주세요.
+* 제4장의 「분석대회용클래스와 폴더의 구성」의 코드에 대해서는[ch04-model-interface/readme.md](ch04-model-interface)을 참고해 주세요.
 
 
 ### Requirements
 
-サンプルコードの動作は、Google Cloud Platform(GCP)で確認しています。  
+샘플코드의 동작은 Google Cloud Platform(GCP)에서 확인했습니다.  
 
-環境は以下のとおりです。
+환경은 아래와 같습니다.
 
 * Ubuntu 18.04 LTS  
 * Anaconda 2019.03 Python 3.7
-* 必要なPythonパッケージ（下記スクリプト参照）
+* 필요한 Python 패키지(하기 스크립트 참조)
 
-以下のスクリプトのとおりにGCPの環境構築を行っています。
+이하의 스크립트대로 GCP의 환경구축을 진행합니다.
 ```
 # utils -----
 
-# 開発に必要なツールをインストール
+# 개발에 필요한 툴을 인스톨
 cd ~/
 sudo apt-get update
 sudo apt-get install -y git build-essential libatlas-base-dev
@@ -45,19 +45,19 @@ sudo apt-get install -y python3-dev
 
 # anaconda -----
 
-# Anacondaをダウンロードしインストール
+# Anaconda를 다운로드하고 인스톨
 mkdir lib
 wget --quiet https://repo.continuum.io/archive/Anaconda3-2019.03-Linux-x86_64.sh -O lib/anaconda.sh
 /bin/bash lib/anaconda.sh -b
 
-# PATHを通す
+# PATH설정
 echo export PATH=~/anaconda3/bin:$PATH >> ~/.bashrc
 source ~/.bashrc
 
 # python packages -----
 
-# Pythonパッケージのインストール
-# numpy, scipy, pandasはAnaconda 2019.03のバージョンのまま
+# Python패키지 인스톨
+# numpy, scipy, pandas는 Anaconda 2019.03 버전 그대로
 # pip install numpy==1.16.2 
 # pip install scipy==1.2.1 
 # pip install pandas==0.24.2
@@ -74,7 +74,7 @@ pip install umap-learn==0.3.9
 
 # set backend for matplotlib to Agg -----
 
-# GCP上で実行するため、matplotlibのbackendを指定し直す
+# GCP상에서 실행하기 때문에 matplotlib의 backend를 다시 지정합니다.
 matplotlibrc_path=$(python -c "import site, os, fileinput; packages_dir = site.getsitepackages()[0]; print(os.path.join(packages_dir, 'matplotlib', 'mpl-data', 'matplotlibrc'))") && \
 sed -i 's/^backend      : qt5agg/backend      : agg/' $matplotlibrc_path
 ```
