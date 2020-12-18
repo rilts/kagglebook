a3d6cbfd5a69cdb020a3dc12306b37259c948396
rilts <rialtoksj@yahoo.com>
2020-12-18 15:54:01 +0900

    2장 첫번째 번역

--- ch02/ch02-01-metrics.py
+++ ch02/ch02-01-metrics.py
@@ -2,13 +2,13 @@ import numpy as np
 import pandas as pd
 
 # -----------------------------------
-# 回帰
+# 회귀
 # -----------------------------------
 # rmse
 
 from sklearn.metrics import mean_squared_error
 
-# y_trueが真の値、y_predが予測値
+# y_true가 실제값, y_pred가 예측값
 y_true = [1.0, 1.5, 2.0, 1.2, 1.8]
 y_pred = [0.8, 1.5, 1.8, 1.3, 3.0]
 
@@ -17,13 +17,13 @@ print(rmse)
 # 0.5532
 
 # -----------------------------------
-# 二値分類
+# 2진분류
 # -----------------------------------
-# 混同行列
+# 오차행렬
 
 from sklearn.metrics import confusion_matrix
 
-# 0, 1で表される二値分類の真の値と予測値
+# 0, 1로 표현되는 2진분류의 실제값과 예측값
 y_true = [1, 0, 1, 1, 0, 1, 1, 0]
 y_pred = [0, 0, 1, 1, 0, 0, 1, 1]
 
@@ -38,7 +38,7 @@ print(confusion_matrix1)
 # array([[3, 1],
 #        [2, 2]])
 
-# scikit-learnのmetricsモジュールのconfusion_matrixでも作成できるが、混同行列の要素の配置が違うので注意が必要
+# scikit-learn의 metrics모듈의 confusion_matrix이라도 작성할 수 있지만 오차행렬의 요소 배치가 다르기에 주의 필요
 confusion_matrix2 = confusion_matrix(y_true, y_pred)
 print(confusion_matrix2)
 # array([[2, 1],
@@ -49,7 +49,7 @@ print(confusion_matrix2)
 
 from sklearn.metrics import accuracy_score
 
-# 0, 1で表される二値分類の真の値と予測値
+# 0, 1로 표현되는 2진분류의 실제값과 예측값
 y_true = [1, 0, 1, 1, 0, 1, 1, 0]
 y_pred = [0, 0, 1, 1, 0, 0, 1, 1]
 accuracy = accuracy_score(y_true, y_pred)
@@ -61,7 +61,7 @@ print(accuracy)
 
 from sklearn.metrics import log_loss
 
-# 0, 1で表される二値分類の真の値と予測確率
+# 0, 1로 표현되는 2진분류의 실제값과 예측확률
 y_true = [1, 0, 1, 1, 0, 1]
 y_prob = [0.1, 0.2, 0.8, 0.8, 0.1, 0.3]
 
@@ -70,13 +70,13 @@ print(logloss)
 # 0.7136
 
 # -----------------------------------
-# マルチクラス分類
+# 멀티클래스 분류
 # -----------------------------------
 # multi-class logloss
 
 from sklearn.metrics import log_loss
 
-# 3クラス分類の真の値と予測値
+# 3클래스 분류의 실제값과 예측값
 y_true = np.array([0, 2, 1, 2, 2])
 y_pred = np.array([[0.68, 0.32, 0.00],
                    [0.00, 0.00, 1.00],
@@ -88,54 +88,54 @@ print(logloss)
 # 0.3626
 
 # -----------------------------------
-# マルチラベル分類
+# 멀티라벨 분류
 # -----------------------------------
 # mean_f1, macro_f1, micro_f1
 
 from sklearn.metrics import f1_score
 
-# マルチラベル分類の真の値・予測値は、評価指標の計算上はレコード×クラスの二値の行列とした方が扱いやすい
-# 真の値 - [[1,2], [1], [1,2,3], [2,3], [3]]
+# 멀티 라벨 분류의 실제값/예측값은 평가지표의 계산상은 레코드×클래스의 2값의 행렬로 하는 편이 다루기 쉽다
+# 실제값 - [[1,2], [1], [1,2,3], [2,3], [3]]
 y_true = np.array([[1, 1, 0],
                    [1, 0, 0],
                    [1, 1, 1],
                    [0, 1, 1],
                    [0, 0, 1]])
 
-# 予測値 - [[1,3], [2], [1,3], [3], [3]]
+# 예측값 - [[1,3], [2], [1,3], [3], [3]]
 y_pred = np.array([[1, 0, 1],
                    [0, 1, 0],
                    [1, 0, 1],
                    [0, 0, 1],
                    [0, 0, 1]])
 
-# mean-f1ではレコードごとにF1-scoreを計算して平均をとる
+# mean-f1에서는 레코드마다 F1-score을 계산하고 평균을 구한다
 mean_f1 = np.mean([f1_score(y_true[i, :], y_pred[i, :]) for i in range(len(y_true))])
 
-# macro-f1ではクラスごとにF1-scoreを計算して平均をとる
+# macro-f1에서는 클래스마다 F1-score를 계산하고 평균을 구한다
 n_class = 3
 macro_f1 = np.mean([f1_score(y_true[:, c], y_pred[:, c]) for c in range(n_class)])
 
-# micro-f1ではレコード×クラスのペアごとにTP/TN/FP/FNを計算し、F1-scoreを求める
+# micro-f1에서는 레코드×클래스의 페어마다 TP/TN/FP/FN을 계산하고 F1-score를 구한다
 micro_f1 = f1_score(y_true.reshape(-1), y_pred.reshape(-1))
 
 print(mean_f1, macro_f1, micro_f1)
 # 0.5933, 0.5524, 0.6250
 
-# scikit-learnのメソッドを使うことでも計算できる
+# scikit-learn의 메소드를 사용해서도 계산할 수 있다.
 mean_f1 = f1_score(y_true, y_pred, average='samples')
 macro_f1 = f1_score(y_true, y_pred, average='macro')
 micro_f1 = f1_score(y_true, y_pred, average='micro')
 
 # -----------------------------------
-# クラス間に順序関係があるマルチクラス分類
+# 클래스간에 순서관계가 있는 멀티 클래스 분류
 # -----------------------------------
 # quadratic weighted kappa
 
 from sklearn.metrics import confusion_matrix, cohen_kappa_score
 
 
-# quadratic weighted kappaを計算する関数
+# quadratic weighted kappa를 계산하는 함수
 def quadratic_weighted_kappa(c_matrix):
     numer = 0.0
     denom = 0.0
@@ -152,39 +152,39 @@ def quadratic_weighted_kappa(c_matrix):
     return 1.0 - numer / denom
 
 
-# y_true は真の値のクラスのリスト、y_pred は予測値のクラスのリスト
+# y_true는 실제값의 클래스 리스트, y_pred는 예측값의 클래스 리스트
 y_true = [1, 2, 3, 4, 3]
 y_pred = [2, 2, 4, 4, 5]
 
-# 混同行列を計算する
+# 오차행렬을 계산한다
 c_matrix = confusion_matrix(y_true, y_pred, labels=[1, 2, 3, 4, 5])
 
-# quadratic weighted kappaを計算する
+# quadratic weighted kappa를 계산한다
 kappa = quadratic_weighted_kappa(c_matrix)
 print(kappa)
 # 0.6153
 
-# scikit-learnのメソッドを使うことでも計算できる
+# scikit-learn의 메소드를 사용해서도 계산할 수 있다
 kappa = cohen_kappa_score(y_true, y_pred, weights='quadratic')
 
 # -----------------------------------
-# レコメンデーション
+# 추천
 # -----------------------------------
 # MAP@K
 
-# K=3、レコード数は5個、クラスは4種類とする
+# K=3, 레코드수는 5개, 클래스는 4종류로 한다.
 K = 3
 
-# 各レコードの真の値
+# 각 레코드의 실제값
 y_true = [[1, 2], [1, 2], [4], [1, 2, 3, 4], [3, 4]]
 
-# 各レコードに対する予測値 - K=3なので、通常は各レコードにそれぞれ3個まで順位をつけて予測する
+# 각 레코드에 대한 예측값 - K=3이므로 일반적으로 각 레코드에 각각 3개까지 순위를 매겨 예측한다
 y_pred = [[1, 2, 4], [4, 1, 2], [1, 4, 3], [1, 2, 3], [1, 2, 4]]
 
 
-# 各レコードごとのaverage precisionを計算する関数
+# 각 레코드마다 average precision을 계산하는 함수
 def apk(y_i_true, y_i_pred):
-    # y_predがK以下の長さで、要素がすべて異なることが必要
+    # y_pred가 K이하의 길이이고 요소가 전부 다른 경우가 필요
     assert (len(y_i_pred) <= K)
     assert (len(np.unique(y_i_pred)) == len(y_i_pred))
 
@@ -200,16 +200,16 @@ def apk(y_i_true, y_i_pred):
     return sum_precision / min(len(y_i_true), K)
 
 
-# MAP@K を計算する関数
+# MAP@K 을 계산하는 함수
 def mapk(y_true, y_pred):
     return np.mean([apk(y_i_true, y_i_pred) for y_i_true, y_i_pred in zip(y_true, y_pred)])
 
 
-# MAP@Kを求める
+# MAP@K을 구하기
 print(mapk(y_true, y_pred))
 # 0.65
 
-# 正解の数が同じでも、順序が違うとスコアも異なる
+# 정답의 수가 같더라고 순서가 다르면 스코어도 다르다
 print(apk(y_true[0], y_pred[0]))
 print(apk(y_true[1], y_pred[1]))
 # 1.0, 0.5833
